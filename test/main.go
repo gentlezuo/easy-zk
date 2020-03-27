a741ec9e44de6143d24f56a29e21428a6c94f953
gentlezuo <15765942275@163.com>
2020-03-27 15:42:53 +0800

    null

--- test/main.go
+++ test/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gentlezuo/easy-zk/zookeeper"
+	"strings"
 	"time"
 )
 
@@ -11,6 +12,11 @@ func main() {
 	fmt.Println(len(s))
 	fmt.Print(len([]rune(s)))
 	strconv.Itoa(4)*/
+	s:="aaa"
+	fmt.Println(len(strings.Split(s,",")))
+	s="bbb,"
+	fmt.Println(len(strings.Split(s,",")))
+	time.Sleep(time.Hour)
 
 	conn, err := zookeeper.Connect([]string{"182.92.99.111:2181"}, time.Second*5)
 	if err != nil {
